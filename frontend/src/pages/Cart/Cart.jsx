8f57a1b11709e2ec8fe305e551f990948afe69ad
Akilanh <akakilan83@gmail.com>
2025-01-29 20:07:12 +0530

    cart page is upade in akilan

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css';
 import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-    const { cartItems = {}, food_list = [], removeFromCart } = useContext(StoreContext);
+    const { cartItem, food_list, removeFromCart } = useContext(StoreContext);
 
     return (
         <div className='cart'>
@@ -19,24 +19,23 @@ const Cart = () => {
                 <br />
                 <hr />
                 {food_list.map((item) => {
-                    const quantity = cartItems[item._id] || 0;
-                    if (quantity > 0) {
+                    if (cartItem[item._id] > 0) {
                         return (
-                            <div key={item._id}>
-                                <div className="cart-items-title cart-items-item">
+                            <><div>
+                                <div key={item._id} className="cart-items-title cart-items-item">
                                     <img src={item.image} alt={item.name} />
                                     <p>{item.name}</p>
                                     <p>{item.price}</p>
-                                    <p>{quantity}</p>
-                                    <p>{item.price * quantity}</p>
+                                    <p>{cartItem[item._id]}</p>
+                                    <p>{item.price * cartItem[item._id]}</p>
                                     <p className="cross" onClick={() => removeFromCart(item._id)}>X</p>
                                 </div>
-                                <hr />
-                            </div>
-                        );
+                            </div><hr /></>
+                        )
                     }
                     return null;
                 })}
+
             </div>
         </div>
     );
